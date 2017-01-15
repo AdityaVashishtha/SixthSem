5443bacd8105a2a8b09498eeb5c2612286f7ec5e
Aditya Vashishtha <vashishthaaditya20@gmail.com>
2017-01-15 23:49:03 +0530

    final complete car parking assignment

--- SystemProgramming/A2_CarPark.c
+++ SystemProgramming/A2_CarPark.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*struct node structure for car regno and time(data)*/
 struct node {
       int data;
       char reg_no[20];
       struct node *prev, *next;
 };
 
+/*initialize array of pointer for 5 lanes and lane car counter for lane capacity*/
 struct node *lane_head[5],*lane_tail[5];
 int lane_count[5]={0};
 int car_count=0;
 
+/*createnode for every insertion*/
 struct node * createNode(int data,char *reg_no) {
       struct node *newnode = (struct node *)malloc(sizeof (struct node));
       newnode->data = data;
@@ -21,6 +24,7 @@ struct node * createNode(int data,char *reg_no) {
       return (newnode);
 }
 
+/*initialize every head and tail pointer for insertions*/
 void createSentinels(int lane) {
       lane_head[lane] = createNode(0,"");
       lane_tail[lane] = createNode(0,"");
@@ -38,7 +42,7 @@ void enqueueAtFront(int data,char *reg_no,int lane) {
       newnode->next = temp;
       temp->prev = newnode;
 }
-
+/*Initialize every lane for insertion and deletion*/
 void initialize(){
       int i=0;
       for(i=0;i<5;i++){
@@ -48,55 +52,44 @@ void initialize(){
       }
 }
 
+/*Car insert at front*/
 void car_insert(int time,char reg[],int lane) {
-      if(car_count<=25)
-            enqueueAtFront(time,reg,lane);
+      if((car_count<=25)) {
+            if((lane_count[lane]<=5)){
+                  enqueueAtFront(time,reg,lane);
+                  lane_count[lane]++;
+            } else {
+                  enqueueAtFront(time,reg,(lane+1)%5);
+            }         
+      }
       else 
             printf("!**Total Capacity Full**!\n");
 }
 
-/*insertion at the rear of the queue */
-void enqueueAtRear(int data,char *reg_no,int lane) {
-      struct node *newnode, *temp;
-      newnode = createNode(data,reg_no);
-      temp = lane_tail[lane]->prev;
-      lane_tail[lane]->prev = newnode;
-      newnode->next = lane_tail[lane];
-      newnode->prev = temp;
-      temp->next = newnode;
-}
-
-/* deletion at the front of the queue */
-void dequeueAtFront(int lane) {
-      struct node *temp;
-      if (lane_head[lane]->next == lane_tail[lane]) {
-            printf("Queue is empty\n");
-      } else {
-            temp = lane_head[lane]->next;
-            lane_head[lane]->next = temp->next;
-            temp->next->prev = lane_head[lane];
-            free(temp);
-      }
-      return;
-}
-
-
-/* deletion at the rear of the queue */
-
-void dequeueAtRear(int lane)  {
-      struct node *temp;
-      if (lane_tail[lane]->prev == lane_head[lane]) {
-            printf("Queue is empty\n");
-      } else {
-            temp = lane_tail[lane]->prev;
-            lane_tail[lane]->prev = temp->prev;
-            temp->prev->next = lane_tail[lane];
-            free(temp);
-      }
-      return;
-}
-
 /* deletion at a special position*/
+void dequeueAtSpecial(char reg[],int lane) {
+      struct node *temp;
+      printf("%d\n",lane);
+      if (lane_head[lane]->next==lane_tail[lane]){
+            printf("Queue is empty\n");
+      } else {
+          temp = lane_head[lane]->next;
+          while((temp!=lane_tail[lane])) {
+                if(strcmp(temp->reg_no,reg)==0){
+                      temp->prev->next = temp->next;
+                      temp->next->prev = temp->prev;
+                      temp->next=NULL;
+                      temp->prev=NULL;
+                      free(temp);
+                      car_count--;
+                      lane_count[lane]--;
+                      printf("Car DEPARTED\n");
+                      break;
+                }
+                temp = temp->next;
+          }
+      }
+}
 
 /* display elements present in the queue */
 void display(int lane) {
@@ -109,12 +102,13 @@ void display(int lane) {
       
       temp = lane_tail[lane]->prev;
       while(temp!=lane_head[lane]) {
-            printf("((%s|]\t ", temp->reg_no);
+            printf("((%s|] ~ ", temp->reg_no);
             temp= temp->prev;
       }
       printf("\n");
 }
 
+/*search a car in every lane sequentialy and then return lane no*/
 int search(char reg[]) {
      struct node *temp;
      int i,flag=0,lane;
@@ -133,6 +127,8 @@ int search(char reg[]) {
                         break;
                   }
                   temp= temp->prev;
+            } if(flag==1) {
+                  break;
             }
      } if(flag==0) {
            printf("no vehical found\n");
@@ -141,8 +137,9 @@ int search(char reg[]) {
      return lane;
 }
 
+/*display current parking lanes*/
 void show_parking() {
-      int i=0;      
+      int i=0;            
       for(i=0;i<5;i++) {
             display(i);
       }      
@@ -150,21 +147,28 @@ void show_parking() {
 
 int menu() {
       int choice;
-      printf("\nPress 0 For Exit\n");
-      printf("Enter Your Choice\n");
-      printf("1. Show Parking Lanes\n");
-      printf("2. Show Specific Lane\n");
-      printf("3. Add a New Car\n");
-      printf("4. Remove a Car\n");
-      printf("5. Search a Car\n");
+      printf("\ncontinue(1),exit(0)\n");      
       scanf("%d",&choice);
-      return choice;
+      if(choice==1) {
+            printf("______________________________________________________\n");
+            printf("\nPress 0 For Exit\n");
+            printf("Enter Your Choice\n");
+            printf("1. Show Parking Lanes\n");
+            printf("2. Show Specific Lane\n");
+            printf("3. Add a New Car\n");
+            printf("4. Remove a Car\n");
+            printf("5. Search a Car\n");
+            printf("______________________________________________________\n");
+            scanf("%d",&choice);      
+      }      
+      return choice;      
 }
 
+/*remove car first search than remove car*/
 void remove_car(char reg[]) {
       int lane=search(reg);
       if(lane>=0){
-            dequeueAtSpecial(char reg,int lane);
+            dequeueAtSpecial(reg,lane);
       }
 }
 
