bb75b6e0a5a9714388de6f6f08c8c89bcb10c4a2
Aditya Vashishtha <vashishthaaditya20@gmail.com>
2017-02-19 17:07:57 +0530

    ClosestPair programm now return closest point value with distance

--- DAA/Assignment3AlgoDesign/ClosestPair.py
+++ DAA/Assignment3AlgoDesign/ClosestPair.py
@@ -8,16 +8,21 @@ def distance(x,y):
     return pow(((x1-x2)**2 + (y1-y2)**2),0.5)
 
 def minDistance(a,l,r):
-    min = LOCAL_MAX
+    min = LOCAL_MAX   
+    x = [0,0]    
+    y = [0,0]
     for i in range(l,r):
         for j in range(i+1,r):
             dist = distance(a[i],a[j])            
             if dist < float(min) :
                 min = dist
-    return min
+                x=a[i]
+                y=a[j]
+    
+    return min,x,y
 
-def minimumToMid(a,l,r,dmin):
-    min = dmin
+def minimumToMid(a,l,r,dmin,x,y):
+    min = dmin    
     a = sorted(a,key= lambda a:a[1])
     for i in range(l,r):
         for j in range(i+1,r):
@@ -27,35 +32,48 @@ def minimumToMid(a,l,r,dmin):
                 dist = distance(a[i],a[j])
                 if dist < min :
                     min = dist
-    return min
+                    x = a[i]
+                    y = a[j]
+    return min,x,y
 
 
 def closest(a,l,r):
     #print minDistance(a,l,r)
     #termination condition
+    x = [0,0]    
+    y = [0,0]
     if (r-l) <=3:
         return minDistance(a,l,r)
 
     mid = l+(r-l)/2
     midX = a[mid]
     #calculating minimum of left and right
-    dMinL = closest(a,l,mid)
-    dMinR = closest(a,mid+1,r)
+    dMinL,x1,y1 = closest(a,l,mid)
+    dMinR,x2,y2 = closest(a,mid+1,r)
     dmin = min(dMinL,dMinR)
+    if(dmin==dMinL):
+        x,y = x1,y1
+    else:
+        x,y = x2,y2
     #calculating middle minimum
     middle =[]
     for i in range(l,r):
         if abs(a[i][0]-midX[0]) < dmin:
             middle.append(a[i])
     #comparing left minimum and right minimum to minimum of Mid
-    midMin = minimumToMid(middle,0,len(middle),dmin)  
-    return min(dmin,midMin)
+    midMin,x,y = minimumToMid(middle,0,len(middle),dmin,x,y)  
+    if (dmin < midMin):
+        return dmin,x,y
+    else :
+        return midMin,x,y
     
 
 
 def closestStart(a):
     a = sorted(a)
-    print closest(a,0,len(a))
+    d,x,y= closest(a,0,len(a))
+    print "Closes Point are (",x[0],",",x[1],") and (",y[0],",",y[1],")"
+    print "With Distance :: ",d
 
 
 print "Enter number of points to enter::"
